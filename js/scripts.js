452dadbc99f2d92a796f12b3645e2f515632e518
qizhangwen <qizhangwen@126.com>
2018-08-18 15:01:24 +0800

    工作经验弹窗

--- js/scripts.js
+++ js/scripts.js
@@ -36,14 +36,22 @@ $('.onLogin').on('click', function () {
 	$('.mask').fadeIn(300);
 	$('.login-popup').fadeIn(300);
 	$('body').css({overflow:'hidden'});
+    $("body,html").scrollTop(0);
+    clMask(0);
 })
 
-$('.mask').on('click', function () {
-	$(this).fadeOut(300);
-	$('.popup').fadeOut(300);
-	$('body').css({overflow:'initial'});
-});
+// 点击蒙版
+function clMask(h){
+    $('.mask').on('click', function () {
+        $(this).fadeOut(300);
+        $('.popup').fadeOut(300);
+        $('.work-popup').fadeOut(300);
+        $('body').css({overflow:'visible'});
+        $("body,html").scrollTop(h);
+    });
+}
 
+// 登录按钮
 $('.register').on('click', function () {
 	$('.mask').hide();
 	$('.popup').hide();
@@ -52,6 +60,17 @@ $('.register').on('click', function () {
 
 // 抽屉
 $('.caption').on('click', function () {
-    console.log(1)
     $('.details').slideToggle('slow');
 });
+
+// 工作经验弹窗
+$('.onPopup').on('click', function () {
+    var thisId = $(this).attr('data-id');
+    var scrollHeight = $(window).scrollTop(); // 当前窗口距离页面顶部高度
+    $('.mask').fadeIn(300);
+    $('.work-popup').fadeIn(300);
+    $('body').css({overflow:'hidden'});
+    $("body,html").scrollTop(scrollHeight);
+    clMask(scrollHeight);
+})
+
